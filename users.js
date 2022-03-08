e01b4a94eb5dc90612a4cb2391c57fdd14d537e3
FeldryansaSomba <s21810003@student.unklab.ac.id>
2022-03-08 23:14:33 +0800

    commit users.js

---
+++ users.js
@@ -0,0 +1,16 @@
+const users = [
+    {
+        id : 1,
+        name: 'Jhon' 
+    },
+    {
+        id : 2,
+        name: 'Smith'
+    },
+    {
+        id : 3,
+        name: 'Bob'
+    },
+]
+
+module.exports = users;
